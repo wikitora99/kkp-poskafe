ff4136e2c017f12137b273d10518d3e053482000
wikitora99 <wiki.tora99@gmail.com>
2022-03-18 18:19:39 +0700

    update dashboard layout

--- public/src/js/deznav-init.js
+++ public/src/js/deznav-init.js
@@ -37,21 +37,7 @@ function getUrlParams(dParam)
 		// containerLayout: "full",
 		// direction: direction
 
-		// CUSTOM THEME 2 (DARK MODE)
-		// typography: "poppins",
-		// version: "dark",
-		// layout: "vertical",
-		// primary: "color_1",
-		// headerBg: "color_1",
-		// navheaderBg: "color_1",
-		// sidebarBg: "color_1",
-		// sidebarStyle: "full",
-		// sidebarPosition: "fixed",
-		// headerPosition: "fixed",
-		// containerLayout: "full",
-		// direction: direction
-
-		// CUSTOM THEME 3 (CHANGE PRIMARY COLOR)
+		// CUSTOM THEME
 		typography: "poppins",
 		version: "light",
 		layout: "vertical",
@@ -64,48 +50,6 @@ function getUrlParams(dParam)
 		headerPosition: "fixed",
 		containerLayout: "full",
 		direction: direction
-
-		// CUSTOM THEME 4 (DARK SIDEBAR)
-		// typography: "poppins",
-		// version: "light",
-		// layout: "vertical",
-		// headerBg: "color_1",
-		// primary: "color_2",
-		// navheaderBg: "color_2",
-		// sidebarBg: "color_2",
-		// sidebarStyle: "full",
-		// sidebarPosition: "fixed",
-		// headerPosition: "fixed",
-		// containerLayout: "full",
-		// direction: direction
-
-		// CUSTOM THEME 5 (MODERN SIDEBAR STYLE)
-		// typography: "poppins",
-		// version: "light",
-		// layout: "vertical",
-		// primary: "color_7",
-		// headerBg: "color_1",
-		// navheaderBg: "color_7",
-		// sidebarBg: "color_1",
-		// sidebarStyle: "modern",
-		// sidebarPosition: "static",
-		// headerPosition: "fixed",
-		// containerLayout: "full",
-		// direction: direction
-
-		// CUSTOM THEME 6 (HORIZONTAL SIDEBAR LAYOUT)
-		// typography: "poppins",
-		// version: "light",
-		// layout: "horizontal",
-		// primary: "color_1",
-		// headerBg: "color_1",
-		// navheaderBg: "color_1",
-		// sidebarBg: "color_3",
-		// sidebarStyle: "full",
-		// sidebarPosition: "static",
-		// headerPosition: "fixed",
-		// containerLayout: "full",
-		// direction: direction
 	};
 
 	
--- public/src/js/script.js
+++ public/src/js/script.js
@@ -70,4 +70,161 @@ $(function() {
     Sweetalert Area-end 
   **************************/
 
+  /**************************
+    Apexcharts Area-start
+  **************************/
+  var chartList = function() {
+
+    var earningsChart = function(){
+    var options = {
+      series: [{
+        name: 'Pendapatan',
+        data: [500000, 300000, 400000, 200000, 500000, 200000, 400000, 1500000, 500000, 200000]
+      }, {
+        name: 'Pengeluaran',
+        data: [200000, 400000, 300000, 400000, 200000, 400000, 200000, 300000, 200000, 300000]
+      }],
+      chart: {
+        height: 300,
+        type: 'area',
+        toolbar:{
+          show:false
+        }
+      },
+      colors:["#00ADA3", "#FFAB2D"],
+        dataLabels: {
+          enabled: false
+        },
+        stroke: {
+          curve: 'smooth',
+      width:3
+        },
+    legend:{
+      show:true
+    },
+    grid:{
+      show:false,
+      strokeDashArray: 6,
+      borderColor: '#dadada',
+    },
+    yaxis: {
+      labels: {
+      style: {
+        colors: '#B5B5C3',
+        fontSize: '12px',
+        fontFamily: 'Poppins',
+        fontWeight: 400
+        
+      },
+      formatter: function (value) {
+        let result = null;
+
+        if (value < 1000) {
+          result = value;
+        }else if (value < 1000000) {
+          result = (value/1000) + " rb";
+        }else if (value < 1000000000) {
+          result = (value/1000000) + " jt";
+        }else if (value < 1000000000000) {
+          result = (value/1000000000) + " M";
+        }else {
+          result = (value/1000000000000) + " T";
+        }
+
+        return result;
+      }
+      },
+    },
+    xaxis: {
+      categories: ["Week 01","Week 02","Week 03","Week 04","Week 05","Week 06","Week 07","Week 08","Week 09","Week 10"],
+      labels:{
+        style: {
+          colors: '#B5B5C3',
+          fontSize: '12px',
+          fontFamily: 'Poppins',
+          fontWeight: 400
+        },
+      }
+    },
+    fill:{
+      type:'solid',
+      opacity:0.05
+    },
+    tooltip: {
+      x: {
+        format: 'dd/MM/yy HH:mm'
+      },
+    },
+    };
+
+    var chart = new ApexCharts(document.querySelector("#earnings-chart"), options);
+    chart.render();
+  }
+
+    var productSalesChart = function() {
+      var options = {
+        series: [120, 92, 84, 69, 30, 50],
+        chart: {
+          width: 380,
+          type: 'pie',
+        },
+        labels: ['Produk 1', 'Produk 2', 'Produk 3', 'Produk 4', 'Produk 5', 'Lainnya'],
+        responsive: [{
+          breakpoint: 480,
+          options: {
+            chart: {
+              width: 200
+            },
+            legend: {
+              psition: 'bottom'
+            }
+          }
+        }]
+      };
+      var chart = new ApexCharts(document.querySelector('#product-sales-chart'), options);
+      chart.render();
+    }
+
+    var categorySalesChart = function() {
+      var options = {
+        series: [120, 92, 84, 69, 30, 50],
+        chart: {
+          width: 380,
+          type: 'pie',
+        },
+        labels: ['Kategori 1', 'Kategori 2', 'Kategori 3', 'Kategori 4', 'Kategori 5', 'Lainnya'],
+        responsive: [{
+          breakpoint: 480,
+          options: {
+            chart: {
+              width: 200
+            },
+            legend: {
+              psition: 'bottom'
+            }
+          }
+        }]
+      };
+      var chart = new ApexCharts(document.querySelector('#category-sales-chart'), options);
+      chart.render();
+    }
+
+    return {
+      load:function(){
+        earningsChart();
+        productSalesChart();
+        categorySalesChart();
+      }
+    }
+  }();
+
+  $(window).on('load',function(){
+    setTimeout(function(){
+      chartList.load();
+    }, 1000); 
+  });
+  /**************************
+    Apexcharts Area-end
+  **************************/
+
 });
