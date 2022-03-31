1177f6a1026385a0bb1082f3933457f1ecb41637
bagasarya15 <bagasarya150800@gmail.com>
2022-03-31 19:03:25 +0700

    change routes product

--- app/Http/Controllers/Owner/ProductController.php
+++ app/Http/Controllers/Owner/ProductController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Controllers\Sales;
+namespace App\Http\Controllers\Owner;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\AuthController;
 
-use App\Http\Controllers\Sales\ProductController as Product;
+use App\Http\Controllers\Owner\ProductController as Product;
 
 /*
 |--------------------------------------------------------------------------
