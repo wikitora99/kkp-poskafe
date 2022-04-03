2c41f46cad9c31c0f4721f41b5b70665df07cff0
wikitora99 <wiki.tora99@gmail.com>
2022-04-03 10:41:28 +0700

    update product controller (update method)

--- app/Http/Controllers/Owner/ProductController.php
+++ app/Http/Controllers/Owner/ProductController.php
@@ -54,38 +54,28 @@ class ProductController extends Controller
       return redirect()->back()->with('error', $validator->errors()->first());
     }
 
-    try {
-      DB::beginTransaction();
+    $product = new Product;
+    $product->sku = $request->sku;
+    $product->category_id = $request->category_id;
+    $product->name = $request->name;
+    $product->desc = $request->desc;
+    $product->buy_price = $request->buy_price ?? 0;
+    $product->sell_price = $request->sell_price;
+    $product->has_stock = $request->has_stock;
+    $product->cur_stock = $request->cur_stock ?? 0;
+    $product->min_stock = $request->min_stock ?? 0;
+    $product->is_active = $request->is_active;
 
-      $product = new Product;
-      $product->sku = $request->sku;
-      $product->category_id = $request->category_id;
-      $product->name = $request->name;
-      $product->desc = $request->desc;
-      $product->buy_price = $request->buy_price ?? 0;
-      $product->sell_price = $request->sell_price;
-      $product->has_stock = $request->has_stock;
-      $product->cur_stock = $request->cur_stock ?? 0;
-      $product->min_stock = $request->min_stock ?? 0;
-      $product->is_active = $request->is_active;
-
-      if ($request->hasFile('picture')){
-        $product->picture = Storage::disk('public')->putFile('product', $request->file('picture'));
-      }else{
-        $product->picture = 'product/default.jpg';
-      }
-
-      $product->save();
-
-      DB::commit();
-
-      return redirect()->route('product.index')
-                        ->with('success', 'Berhasil menambahkan produk baru!');
-    } catch (Exception $e) {
-      DB::rollback();
-
-      return redirect()->back()->with('error', $e->getMessage());
+    if ($request->hasFile('picture')){
+      $product->picture = Storage::disk('public')->putFile('product', $request->file('picture'));
+    }else{
+      $product->picture = 'product/default.jpg';
     }
+
+    $product->save();
+
+    return redirect()->route('product.index')
+                      ->with('success', 'Berhasil menambahkan produk baru!');
   }
 
 
@@ -104,7 +94,52 @@ class ProductController extends Controller
   /** Update the specified resource in storage. **/
   public function update(Request $request, Product $product)
   {
-    // dd($request);
+    if ($request->name != $product->name){
+      $name_rules = ['required', 'string', 'max:255', 'unique:products,name'];
+    }else{
+      $name_rules = ['required'];
+    }
+
+    $validator = Validator::make($request->all(), [
+      'name' => $name_rules,
+      'category_id' => 'required|integer|exists:product_categories,id',
+      'picture' => 'file|image|mimes:jpg,png|max:1024',
+      'desc' => 'nullable|string|max:255',
+      'is_active' => 'required|boolean',
+      'buy_price' => 'nullable|integer',
+      'sell_price' => 'required|integer',
+      'has_stock' => 'required|boolean|declined_if:cur_stock,0',
+      'cur_stock' => 'required_if:has_stock,1|integer',
+      'min_stock' => 'required_if:has_stock,1|integer'
+    ]);
+
+    if ($validator->fails()){
+      return redirect()->back()->with('error', $validator->errors()->first());
+    }
+
+    $product->category_id = $request->category_id;
+    $product->name = $request->name;
+    $product->desc = $request->desc;
+    $product->buy_price = $request->buy_price ?? 0;
+    $product->sell_price = $request->sell_price;
+    $product->has_stock = $request->has_stock;
+    $product->cur_stock = $request->cur_stock ?? 0;
+    $product->min_stock = $request->min_stock ?? 0;
+    $product->is_active = $request->is_active;
+
+    if ($request->hasFile('picture')){
+      if ($product->picture != 'product/default.jpg'){
+        Storage::disk('public')->delete($product->picture);
+      }
+      $product->picture = Storage::disk('public')->putFile('product', $request->file('picture'));
+    }
+
+    if (!$product->isDirty()){
+      return redirect()->back()->with('warning', 'Tidak ada data yang diubah!');
+    }
+
+    $product->push();
+
     return redirect()->back()->with('success', 'Data produk berhasil diubah!');
   }
 
